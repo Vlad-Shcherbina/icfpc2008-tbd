4d5f5f1873b740e1298c9440cfdee0d1d5a183e0
irusskih <irusskih@cc3fe7c6-f534-0410-a7bd-bf3b583398bf>
2008-07-12 07:35:05 +0000

    vbox instructions

--- runsim.bat
+++ runsim.bat
@@ -1,3 +1,10 @@
 set MYIP=169.254.149.37
+
+:restart
+
+rem ssh -X knoppix@knoppix killall -9 server
+
 ssh -X knoppix@knoppix export DISPLAY=%MYIP%:0.0;/media/hda1/2008/sim/lo-res/server -v /media/hda1/2008/sim/sample-maps/simple-small.wrld
 pause
+
+goto restart
