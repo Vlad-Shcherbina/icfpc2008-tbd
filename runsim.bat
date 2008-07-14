80eced73e1fabdc008d76ed26b30d865f3297e9c
irusskih <irusskih@cc3fe7c6-f534-0410-a7bd-bf3b583398bf>
2008-07-14 13:52:47 +0000

    distr script

--- runsim.bat
+++ runsim.bat
@@ -1,10 +1,26 @@
 set MYIP=169.254.149.37
+set PATH=C:\programs\tools\nircmd;%PATH%
 
 :restart
 
-rem ssh -X knoppix@knoppix killall -9 server
+echo restarting
 
-ssh -X knoppix@knoppix export DISPLAY=%MYIP%:0.0;/media/hda1/2008/sim/lo-res/server -v /media/hda1/2008/sim/sample-maps/simple-small.wrld
-pause
+nircmdc.exe sendkey 1 press
+nircmdc.exe sendkey 2 press
+nircmdc.exe sendkey 3 press
+nircmdc.exe sendkey enter press
+ssh -X knoppix@knoppix killall -9 server
+rem pause
 
-goto restart
+
+nircmdc.exe sendkey 1 press
+nircmdc.exe sendkey 2 press
+nircmdc.exe sendkey 3 press
+nircmdc.exe sendkey enter press
+rem ssh -X knoppix@knoppix export DISPLAY=%MYIP%:0.0;/media/hda1/2008/sim/lo-res/server -v /media/hda1/2008/sim/sample-maps/%1
+rem ssh -X knoppix@knoppix export DISPLAY=%MYIP%:0.0;/media/hda1/2008/sim/linux2.6.24/server -v -p 17676 /media/hda1/2008/sim/sample-maps/%1
+ssh -X knoppix@knoppix export DISPLAY=%MYIP%:0.0;/media/hda1/2008/sim/livecd/server -v -p 17676 /media/hda1/2008/sim/sample-maps/%1
+
+exit
+
+rem goto restart
